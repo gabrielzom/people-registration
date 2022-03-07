23c43f5d6036bba0ee2ccdb8df276e7ca420cfe6
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2022-03-07 16:57:40 -0300

    add dependencies and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+#Persons Register
+
+Simple application for registering people. Used as "api service", but based on the mvc pattern, to complement a desktop application.
+
+It suis base for many others applications.
--- app.js
+++ app.js
@@ -3,23 +3,26 @@ const express = require("express");
 //const home    = require("./routes/home")
 //const usuario = require("./routes/usuario")
 //const send    = require("./routes/send")
-//const methodOverride = require("method-override");
-//const session = require("express-session")
-//const flash = require("connect-flash")
+const methodOverride = require("method-override");
+const session = require("express-session")
+const flash = require("connect-flash")
 const app = express();
-//const passport = require("passport")
+const passport = require("passport")
 const fs = require("fs")
+
 var um_dia = 86400000;
+var port = process.env.PORT || 9090
 
 //require("./config/auth")(passport)
 require("dotenv").config();
 
-/*app.use(session({
+app.use(session({
   secret : process.env.KEY,
   resave : true,
   saveUninitialized : true,
   cookie : { maxAge : um_dia }
 }))
+
 
 app.use(passport.initialize())
 app.use(passport.session())
@@ -33,12 +36,15 @@ app.use((req, res, next) => {
   next()
 })
 
-
 app.use(express.static("public"))
 app.use(methodOverride("_method"))
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 
+app.set('view engine', 'ejs')
+app.set('views', ('./views'))
+
+/*
 app.use("/usuario", usuario)
 app.use("/usuario/excluir", usuario)
 app.use("/usuario/redefinir", usuario)
@@ -53,11 +59,8 @@ app.use("/clientes/editar", cliente)
 app.use("/clientes/excluir", cliente)
 app.use("/", home)
 app.use("/send", send)
-
-app.set('view engine', 'ejs')
-app.set('views', ('./views'))
 */
-let port = process.env.PORT || 9090
+
 
 app.listen(port, () => {
   console.log(`App listen in port: http://localhost:${port}`)
