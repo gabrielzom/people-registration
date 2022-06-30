70ccc69353d1bf5d101ee9e2cbe8d704a4520d13
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2022-06-29 23:42:22 -0300

    refactor/feature: add class for guard response messages

---
+++ utils/ResponseMessages.js
@@ -0,0 +1,10 @@
+export default class ResponseMessages {
+  static empty = "";
+  static emailAlreadyInUse = "Este endereço de e-mail já está sendo usado. ";
+  static userIncludeSuccessfully = "Usuário incluído com sucesso. Peça ao novo usuário que acesse seu e-mail e confirme o cadastro.";
+  static notDeleteAuthenUser = "Não é possível deletar um usuário autenticado. ";
+  static notDeleteAdminUser = "Não é possível deletar um usuário administrador. ";
+  static userNotFound = "Este e-mail de usuário não existe";
+  static recoveryPassLinkInvalid = "Link de recuperação de senha expirado ou inválido.";
+  static recoveryPassdNotAutorized = "Usuário não solicitou recuperação de senha e/ou não autorizado. ";
+}
